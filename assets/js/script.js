c3f06f37fba77642b9bc0cf61925e1fcf0f2a47b
Ken Ilo <keneilo91@yahoo.com>
2022-01-23 23:19:34 -0600

    created DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('#form1');
+const buttonEl = document.querySelector('#btn');
+const clearHistoryButtonEl = document.querySelector('#clear-history'); 
+const historyEl = document.querySelector('#history');
+const cityEl = document.querySelector('#city');
+const icon = document.querySelector('#icon');
+const tempEl = document.querySelector('#temperature');
+const humEl = document.querySelector('#humidity');
+const windEl = document.querySelector('#uv');
+const fiveDayForecast = document.querySelector('#daily-forecast');
+
