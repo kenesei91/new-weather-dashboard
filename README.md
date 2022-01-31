203f7a8bcd52072c1be912879a00ff435a8d34a1
Ken Ilo <keneilo91@yahoo.com>
2022-01-30 19:59:57 -0600

    cleanup and readme

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # new-weather-dashboard
+
+Javascript Challenge
+
+**Version 1.5.0**
+
+**Weather Dashboard App challenge activity.**
+
+The aim of this project is to create a simple weather dashborad application that allows a user to search for a city's weather forecast and save those searched cities.
+
+This project focuses on the use of javascript to create this application.
+
+## Acceptance criteria
+
+- Given a weather dashboard with form inputs, when I search for a city, then I am presented with current and future conditions for that city and that city is added to the search history.
+- When I view current weather conditions for that city, then I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index.
+- When I view the UV index, then I am presented with a color that indicates whether the conditions are favorable, moderate, or severe.
+- When I view future weather conditions for that city, then I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
+- When I click on a city in the search history, then I am again presented with current and future conditions for that city.
+
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+- Bootstraps
+- Material design bootstrap
+- Moment.js
+- Open Weather API
+
+
+## Work Completed
+
+**A successful completion of this project includes the following:**
+
+- [x]Use of loops and if, else, esleif, and while statements.
+- [x]Application of arrays
+- [x]Use of DOM
+- [x]Creating multiple variables and functions
+- [x]Applying queryselectors to retrieve information
+- [x]Applying bootstraps
+- [x]Applying local storage to retrieve information
+
+
+## Project Visual
+
+![Project-Picture](./assets/images/screenshot.png)
+- [x]Showing the current date at the top of the calender app.
+
+## Github pages and links
+
+This project has been deployed to GitHub Pages. 
+- [Deployed application](https://kenesei91.github.io/new-weather-dashboard/)
+
+- [Github Repository](https://github.com/kenesei91/new-weather-dashboard)
+
+
+## Contributors
+
+- Kenechukwu K Ilochonwu <keneilo91@yahoo.com>
+
+
+## Licence & Copyright
+
+
+© Kenechukwu K Ilochonwu, Web Project Design
+
+
+Licensed under the [LICENSE] (MIT LICENSE)
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,12 @@ var uvEl = document.querySelector("#uv");
 var fiveDayContainer = document.querySelector(".five-day-container");
 var fiveDayContainers = document.querySelector(".five-day-container-col");
 
+var containerBorder = document.querySelector(".container-border");
+
+//set searched cities into empty array
+//let cities = [];
+
+// Formhandler to search for particular city
 var formSubmitHandler = function (event) {
   event.preventDefault();
   var city = cityInputEl.value.trim();
@@ -20,12 +26,13 @@ var formSubmitHandler = function (event) {
   if (city) {
     currentCityWeather(city);
     fiveDayForecast(city);
+    cityInputEl.value = "";
   } else {
     alert("Please enter a valid city");
   }
 };
 
-// get current city weather
+// API call for CURRENT DAY city weather
 var currentCityWeather = function (city) {
   var apiKey = "3f698036d7cb81fb192ca1a1ad2af845";
   var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
@@ -34,13 +41,12 @@ var currentCityWeather = function (city) {
     response.json().then(function (data) {
       console.log(data);
       current_forecast_city(data, city);
-      
     });
   });
 };
 
-// get five day forecast
-var fiveDayForecast = function(city) {
+// API call for FIVE DAY forecast
+var fiveDayForecast = function (city) {
   var apiKey = "3f698036d7cb81fb192ca1a1ad2af845";
   var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
@@ -50,62 +56,91 @@ var fiveDayForecast = function(city) {
       five_forecast(data);
     });
   });
-}
+};
 
+// get current city weather
 var current_forecast_city = function (forecast) {
-  currentForecastCity.textContent = `${forecast.name }`;
+  currentForecastCity.textContent = `${forecast.name}`;
   tempEl.textContent = `Temperature: ${forecast.main.temp} °F`;
   windEl.textContent = `Wind speed: ${forecast.wind.speed} MPH`;
   humEl.textContent = `Wind speed: ${forecast.main.humidity} %`;
 
   var forecastDate = document.createElement("span");
-  forecastDate.textContent = ` (${moment(forecast.dt.value).format("MMM D, YYYY")})`;
+  forecastDate.textContent = ` (${moment(forecast.dt.value).format("l")})`;
   currentForecastCity.appendChild(forecastDate);
 
   var forecastIcon = document.createElement("img");
-  forecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`);
+  forecastIcon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
+  );
   currentForecastCity.appendChild(forecastIcon);
 
   var lat = forecast.coord.lat;
   var lon = forecast.coord.long;
 };
 
-var five_forecast = function(forecast){
+// get 5 day forecast to display in cards
+var five_forecast = function (forecast) {
   fiveDayContainer.textContent = "";
-  dailyForecastTitle.textContent = "5 Day Forecast";
+  dailyForecastTitle.textContent = "5-Day Forecast";
 
   var forecastList = forecast.list;
-  for (var i=5; i < forecastList.length; i=i+8){
+  for (var i = 5; i < forecastList.length; i = i + 8) {
     var forecastListBox = forecastList[i];
     console.log(forecastListBox);
 
     var forecastDay = document.createElement("div");
     forecastDay.classList = "card bg-info m-2";
 
-    var forecast5Date = document.createElement("h4");
-    forecast5Date.textContent = moment.unix(forecastListBox.dt).format("MMM D, YYYY");
+    var forecast5Date = document.createElement("h5");
+    forecast5Date.textContent = moment
+      .unix(forecastListBox.dt)
+      .format("MMM D, YYYY");
     forecast5Date.classList = "card-header text-center text-white";
     forecastDay.appendChild(forecast5Date);
     console.log(forecast5Date);
 
     var forecastIcon = document.createElement("img");
     forecastIcon.classList = "card-image";
-    forecastIcon.setAttribute("src", `https://openweathermap.org/img/wn/${forecastListBox.weather[0].icon}@2x.png`);
-    forecastDay.appendChild(forecastIcon); 
+    forecastIcon.setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${forecastListBox.weather[0].icon}@2x.png`
+    );
+    forecastDay.appendChild(forecastIcon);
 
-    var forecastTemp=document.createElement("span");
+    var forecastTemp = document.createElement("span");
     forecastTemp.classList = "card-footer text-center text-white";
     forecastTemp.textContent = forecastListBox.main.temp + " °F";
     forecastDay.appendChild(forecastTemp);
 
-    var forecastHum=document.createElement("span");
+    var forecastHum = document.createElement("span");
     forecastHum.classList = "card-text text-center text-white";
     forecastHum.textContent = forecastListBox.main.humidity + "  %";
     forecastDay.appendChild(forecastHum);
 
     fiveDayContainer.appendChild(forecastDay);
   }
+};
 
-}
+var pastSearch = function (pastSearch) {
+  // console.log(pastSearch)
+
+  pastSearchEl = document.createElement("button");
+  pastSearchEl.textContent = pastSearch;
+  pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+  pastSearchEl.setAttribute("data-city", pastSearch);
+  pastSearchEl.setAttribute("type", "submit");
+
+  pastSearchButtonEl.prepend(pastSearchEl);
+};
+
+var pastSearchHandler = function (event) {
+  var city = event.target.getAttribute("data-city");
+  if (city) {
+    getCityWeather(city);
+    get5Day(city);
+  }
+};
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
